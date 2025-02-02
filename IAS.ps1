36f221cd5977f1d0903464e1840426dd0e4ef634
Saurav Kumar <0501saurav@gmail.com>
2025-02-02 15:43:55 +0530

    Update IAS.ps1

--- IAS.ps1
+++ IAS.ps1
@@ -1,28 +1,25 @@
-# Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
+# Enable TLSv1.2 for security
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+# Corrected RAW file URL
+$DownloadURL = 'https://raw.githubusercontent.com/Astro-Saurav/IDM-Activation-Script/main/IAS.cmd'
 
-$DownloadURL = 'https://github.com/Astro-Saurav/-IDM-Activation-Script/main/IAS.cmd'
-
+# Generate a random filename
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IAS_$rand.cmd" }
 
+# Try downloading the file
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+} catch {
+    Write-Host "Failed to download the script. Check the URL!"
+    exit
 }
-catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
-}
 
-$ScriptArgs = "$args "
-$prefix = "@REM $rand `r`n"
-$content = $prefix + $response
-Set-Content -Path $FilePath -Value $content
+# Save the script and execute it
+Set-Content -Path $FilePath -Value $response.Content
+Start-Process -FilePath $FilePath -Wait
 
-Start-Process $FilePath $ScriptArgs -Wait
-
-$FilePaths = @("$env:TEMP\IAS*.cmd", "$env:SystemRoot\Temp\IAS*.cmd")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+# Cleanup
+Remove-Item -Path $FilePath -Force
